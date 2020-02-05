dde22ff8370dc9b239cded4ccc8d5a4db223445a
nlarsen7 <nlarsen2013@gmail.com>
2020-02-05 03:19:20 +0000

    Initial commit

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+fmt.Print("This is Math Magic!!")
+var answer= 37
+var factor= 99-answer
+fmt.Print("enter a number between 50-99")
+var guess= factor
+
+
+}
